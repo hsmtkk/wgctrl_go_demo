c541fd577207348486f82593360c9114a5ab03de
kouki <kouki@localhost>
2021-01-20 15:45:21 +0900

    separate function

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import(
 	"fmt"
 	"log"
 	"golang.zx2c4.com/wireguard/wgctrl"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 )
 
 func main(){
@@ -11,11 +12,24 @@ func main(){
 	if err != nil {
 		log.Fatal(err)
 	}
+	listDevices(clt)
+}
+
+func listDevices(clt *wgctrl.Client){
 	devices,err := clt.Devices()
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, dev := range devices {
-		fmt.Printf("%v\n", dev)
+		printDevice(dev)
 	}
+}
+
+func printDevice(dev *wgtypes.Device){
+	fmt.Printf("Name: %s\n", dev.Name)
+	fmt.Printf("Type: %s\n", dev.Type)
+	fmt.Printf("Private key: %s\n", dev.PrivateKey)
+	fmt.Printf("Public Key: %s\n", dev.PublicKey)
+	fmt.Printf("Listen Port: %d\n", dev.ListenPort)
+	fmt.Printf("Peers: %s\n", dev.Peers)
 }
